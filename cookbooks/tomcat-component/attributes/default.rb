0860d874f8646424f34b520902234084fa38d568
Artem Ivantsov <loky@Artems-MacBook-Pro.local>
2014-04-05 01:13:17 +0300

    add cookboks as modules; add cookbooks dir

---
+++ cookbooks/tomcat-component/attributes/default.rb
@@ -0,0 +1,7 @@
+default['tomcat-component']['lib_uri'] = ""
+default['tomcat-component']['war_uri'] = ""
+default['tomcat-component']['create_context'] = true
+default['tomcat-component']['war']['uri'] = ""
+default['tomcat-component']['war']['appname'] = ""
+default['tomcat-component']['context'] = nil
+
