f1b9edbc06818e14a2d781b2732e891b68be80a6
Artem Ivantsov <loky@Artems-MacBook-Pro.local>
2014-04-15 15:40:55 +0300

    remove components cookboks ;update  component-broadleaf-solrCloud.yml

--- cookbooks/tomcat-component/attributes/default.rb
+++
@@ -1,7 +0,0 @@
-default['tomcat-component']['lib_uri'] = ""
-default['tomcat-component']['war_uri'] = ""
-default['tomcat-component']['create_context'] = true
-default['tomcat-component']['war']['uri'] = ""
-default['tomcat-component']['war']['appname'] = ""
-default['tomcat-component']['context'] = nil
-
