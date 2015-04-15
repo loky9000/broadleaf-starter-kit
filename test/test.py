9be64cab82fa0a4f8a8918c9e454324b4a5c59a0
Artem Ivantsov <aivantsov@qubell.com>
2015-04-15 11:49:08 +0300

    add test for switching on external solr

--- test/test.py
+++ test/test.py
@@ -63,22 +63,12 @@ class BroadleafTestCase(BaseComponentTestCase):
 
         assert result == 0
     
-    @instance(byApplication=name)
-    def test_solr_search(self, instance):
-         hosts = instance.returnValues['broadleaf.Solr-urls']
-         
-         for host in hosts:
-	     resp = requests.get(host + "/select/?q=*:*", verify=False)
-             assert resp.status_code == 200
-
-    @instance(byApplication=name)
-    def test_broadleaf_up(self, instance):
-        check_site(instance)
 
     @instance(byApplication=name)
     def test_scaling(self, instance):
         assert len(instance.returnValues['broadleaf.Application_hosts']) == 1
-        params = {'configuration.clusterSize': '2'}
+        params = {'configuration.clusterSize': '2',
+                 'configuration.SolrclusterSize': instance.parameters['configuration.SolrclusterSize']}
         instance.reconfigure(parameters=params)
         assert instance.ready(timeout=45)
 
@@ -88,4 +78,18 @@ class BroadleafTestCase(BaseComponentTestCase):
         def eventually_assert():
             assert len(instance.returnValues['broadleaf.Application_hosts']) == 2
         eventually_assert()
+    @instance(byApplication=name)
+    def test_add_solr(self, instance):
+        params = {'configuration.SolrclusterSize': 1,
+                  'configuration.clusterSize': instance.parameters['configuration.clusterSize']}
+        instance.reconfigure(parameters=params)
+        assert instance.ready(timeout=40)
+        hosts = instance.returnValues['broadleaf.Solr-urls']
 
+        for host in hosts:
+            resp = requests.get(host + "/select/?q=*:*", verify=False)
+            assert resp.status_code == 200
+
+    @instance(byApplication=name)
+    def test_broadleaf_up(self, instance):
+        check_site(instance)  
